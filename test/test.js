70249f68ad7c3a543f70ef741f0f152a9330147b
hd21 <hazelmarie.doctor@gmail.com>
2017-07-24 12:35:19 -0400

    Modified test file

--- test/test.js
+++ test/test.js
@@ -3,11 +3,52 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const faker = require('faker');
-const Entry = mongoose.model('Entry');
 
 const should = chai.should();
 const expect = chai.expect;
 
-const { app, runServer, closeServer } = require('./server');
+const Entry = mongoose.model('Entry');
+const { app, runServer, closeServer } = require('../server');
+const { TEST_DATABASE_URL } = require('dotenv').config({ path: 'variables.env' });
 
 chai.use(chaiHttp);
+
+const seedEntryData = () => {
+  console.info('Seeding Entry Data');
+  const seedData = [];
+
+  for (let i = 1, i < 5, i++) {
+    seedData.push(createEntryData());
+  }
+  return Entry.insertMany(seedData);
+
+};
+
+const createEntryData = () => {
+  return {
+    title: faker.random.words(),
+    entered: faker.date.recent(),
+    photo: faker.image.image(),
+    description: faker.lorem.sentences()
+  };
+};
+
+const tearDownDb = () => {
+  console.log('Tearing down database!');
+  return mongoose.connection.dropDatabase();
+};
+
+describe('Entries API Resource', function() {
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+  beforeEach(function() {
+    return seedEntryData;
+  });
+  afterEach(function(){
+    return tearDownDb();
+  });
+  after(function(){
+    return closeServer();
+  });
+});
